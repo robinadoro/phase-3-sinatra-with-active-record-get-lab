a8b98ffabeaabed7aa4ab9bccae27a6f6b1bb74d
Robinson Idagiza <robinadoro@gmail.com>
2022-06-23 16:08:56 +0300

    ft:completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    # "Hello World"
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  # returns a single bakery as JSON with its baked goods nested in an array
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+  
+  # returns an array of baked goods as JSON, sorted by price in descending order
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
   # add routes
 
 end
